efca49536735eb311ae058391fe5385925f0fb1b
Marcosh22 <marcos.henriquerds@gmail.com>
2024-09-10 11:21:22 -0300

    chore: change restaurant api endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { Menu } from "@/@types/menuTypes";
 import { Restaurant } from "@/@types/restaurantTypes";
 
 export const fetchRestaurantDetails = async (): Promise<Restaurant> => {
-  const response = await fetch("/api/restaurant");
+  const response = await fetch("https://cdn-dev.preoday.com/challenge/venue/9");
   const data: Restaurant = await response.json();
   return data;
 };
