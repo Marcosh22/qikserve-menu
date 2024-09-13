fd31833548d1a3808d519d55ce3b13ba34d17db1
Marcosh22 <marcos.henriquerds@gmail.com>
2024-09-13 01:06:34 -0300

    fix: sticky component resize bug

--- src/components/Sticky/Sticky.tsx
+++ src/components/Sticky/Sticky.tsx
@@ -12,47 +12,60 @@ function Sticky({ children }: { children: ReactNode }) {
   useEffect(() => {
     const handleScroll = () => {
       const stickyElement = stickyRef.current;
-
-      if(!stickyElement) return;
-
+      if (!stickyElement) return;
+  
       const parent = stickyElement.parentElement;
-
-      if(!parent) return;
-
+      if (!parent) return;
+  
       const parentTop = parent.getBoundingClientRect().top;
       const stickyPoint = window.innerWidth >= 768 ? 52 : 50;
-
-      if (parentTop <= stickyPoint && !isSticky) {
-        const { left, width, height } = stickyElement.getBoundingClientRect();
-
-        if(wrapperRef?.current) wrapperRef.current.style.height = `${height}px`;
-        
+      const isPastStickyPoint = parentTop <= stickyPoint;
+  
+      if (wrapperRef?.current) {
+        const { height } = stickyElement.getBoundingClientRect();
+        wrapperRef.current.style.height = `${height}px`;
+      }
+  
+      if (isPastStickyPoint && !isSticky) {
+        const { left, width } = parent.getBoundingClientRect();
         setStickyStyles({
-          position: 'fixed',
+          position: "fixed",
           top: `${stickyPoint}px`,
           left: `${left}px`,
           width: `${width}px`,
         });
         setIsSticky(true);
-      } else if (parentTop > stickyPoint && isSticky) {
+      } else if (!isPastStickyPoint && isSticky) {
         setStickyStyles({});
         setIsSticky(false);
+      } else if (isSticky) {
+        const { left, width } = parent.getBoundingClientRect();
+        setStickyStyles({
+          position: "fixed",
+          top: `${stickyPoint}px`,
+          left: `${left}px`,
+          width: `${width}px`,
+        });
       }
     };
-
+  
     handleScroll();
-    window.addEventListener('scroll', handleScroll);
-
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleScroll);
+  
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleScroll);
     };
   }, [isSticky, setIsSticky, setStickyStyles]);
+  
 
   return (
     <div className={styles.sticky} ref={wrapperRef}>
       <div
         className={`${styles.stickyContent} ${isSticky ? styles.fixed : ""}`}
-        ref={stickyRef} style={stickyStyles}
+        ref={stickyRef}
+        style={stickyStyles}
       >
         {children}
       </div>
